4ebd053d872ea4f92f14ec0c7ff084f46731669c
Drew McGowan <michael.mcgowan@students.makeschool.com>
2018-10-15 15:41:51 -0700

    added MongoDB and rides model

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-require('dotenv').config();
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 var exphbs = require('express-handlebars');
+const Ride = mongoose.model('Ride', {
+  start: String,
+  end: String
+});
+
+mongoose.connect('mongodb://localhost/intesive');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+let rides = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  Ride.find()
+    .then(reviews => {
+      res.render('rides-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+// NEW
+app.get('/rides/new', (req, res) => {
+  res.render('rides-new', {});
 })
 
 app.listen(3000, () => {
