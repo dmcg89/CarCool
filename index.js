72cc95242638c5338ef5d33e2fff8e608da23857
Drew McGowan <michael.mcgowan@students.makeschool.com>
2018-10-16 10:51:15 -0700

    routes working

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
-
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const express = require('express');
+const app = express();
 var exphbs = require('express-handlebars');
-const Ride = mongoose.model('Ride', {
-  start: String,
-  end: String
-});
+const bodyParser = require('body-parser');
+const Blog = require('./models/model');
+const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost/intesive');
+
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+
+app.use(methodOverride('_method'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let rides = [
-  { title: "Great Review", movieTitle: "Batman II" },
-  { title: "Awesome Movie", movieTitle: "Titanic" }
-]
+module.exports = app;
+const blogs = require('./controllers/routes')(app, Blog);
 
-// INDEX
-app.get('/', (req, res) => {
-  Ride.find()
-    .then(reviews => {
-      res.render('rides-index', { reviews: reviews });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-})
-
-// NEW
-app.get('/rides/new', (req, res) => {
-  res.render('rides-new', {});
-})
-
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
+app.listen(port, () => {
+    console.log('App Listening on port 3000');
+});
---
+++ models/model.js
@@ -0,0 +1,16 @@
+
+//models/blog.js
+//formally know as models/review.js
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/intensive', { useNewUrlParser: true});
+// const mongoose = require('mongoose');
+
+
+const Blog = mongoose.model('Blog', {
+    title: String,
+    description: String,
+    articleTitle: String,
+    dropDown: Number,
+});
+
+module.exports = Blog;
