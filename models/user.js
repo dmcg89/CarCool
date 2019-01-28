2d05de76589b3b5e9c31b4587c7124542c74dcf3
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-01-28 11:56:47 -0800

    updated user

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   createdAt       : { type: Date },
   updatedAt       : { type: Date },
+  haveCar         : { type: Boolean},
   password        : { type: String, select: false },
   username        : { type: String, required: true }
 });
